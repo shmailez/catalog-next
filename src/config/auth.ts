fb0ff3228b0959a291ca89563c13d9ccd2ad7f6c
Anton Mamontov <a.mamontov@p-s.kz>
2024-10-03 22:50:32 +0500

    config ts

--- src/config/auth.ts
+++ src/config/auth.ts
@@ -1,10 +1,11 @@
+import type { AuthOptions } from 'next-auth'
 import GoggleProvider from 'next-auth/providers/google'
 
-export const authCoufig = {
+export const authCoufig: AuthOptions = {
     providers: [
         GoggleProvider({
-            clientId: process.env.GOOGLE_CLIENT_ID ?? '',
-            clientSecret: process.env.GOOGLE_SECRET ?? ''
+            clientId: process.env.GOOGLE_CLIENT_ID as string,
+            clientSecret: process.env.GOOGLE_SECRET as string
         }),
     ],
 }
