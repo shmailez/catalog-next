6a212895171bd9d267b33d5c60060adad2c48f21
Anton Mamontov <a.mamontov@p-s.kz>
2024-10-20 10:47:52 +0500

    without pre

--- src/components/CatalogItem.tsx
+++ src/components/CatalogItem.tsx
@@ -3,8 +3,10 @@
 import { useState } from "react";
 
 export default function CatalogItemer({ article }: any) {
-  const arr = article.body.split("\n");
-  arr.sort(() => Math.random() - 0.5);
+  const articleSeriatim = article.body.split("\n");
+
+  const articleRandomise = article.body.split("\n");
+  articleRandomise.sort(() => Math.random() - 0.5);
 
   let [toggle, setToggle] = useState(true);
 
@@ -15,14 +17,19 @@ export default function CatalogItemer({ article }: any) {
   return (
     <>
       <h1>{article.title}</h1>
-      <button onClick={() => toggler()}>
+      <button className="deleteArticleButton" onClick={() => toggler()}>
         {toggle ? "перемешать" : "по порядку"}
       </button>
       {toggle ? (
-        <pre>{article.body}</pre>
-      ) : (
         <ul>
-          {arr.map((x: any, i: any) => (
+          {articleSeriatim.map((x: any, i: any) => (
+            <li key={i}>{x}</li>
+          ))}
+        </ul>
+      ) : (
+        // <pre>{article.body}</pre>
+        <ul>
+          {articleRandomise.map((x: any, i: any) => (
             <li key={i}>{x}</li>
           ))}
         </ul>
